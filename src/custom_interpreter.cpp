bf20a89232126f83cb79a7155fd09dadaf195dea
Arne Naegel <naegel@gcsc.uni-frankfurt.de>
2020-05-04 10:17:49 +0200

    LICENSE ref in src

--- src/custom_interpreter.cpp
+++ src/custom_interpreter.cpp
@@ -1,3 +1,11 @@
+/***********************************************************************************                                                               
+* Copyright (c) 2020, Goethe University Frankfurt, G-CSC                           *                                                               
+* Author: Arne Naegel                                                              *                                                               
+*                                                                                  *                                                               
+* Distributed under the terms of the BSD 3-Clause License.                         *                                                               
+*                                                                                  *                                                               
+* The full license is in the file LICENSE, distributed with this software.         *                                                               
+************************************************************************************/
 #include <iostream>
 #include "custom_interpreter.hpp"
 
--- src/custom_interpreter.hpp
+++ src/custom_interpreter.hpp
@@ -1,3 +1,12 @@
+/***********************************************************************************
+* Copyright (c) 2020, Goethe University Frankfurt, G-CSC                           *
+* Author: Arne Naegel                                                              *
+*                                                                                  *
+* Distributed under the terms of the BSD 3-Clause License.                         *
+*                                                                                  *
+* The full license is in the file LICENSE, distributed with this software.         *
+************************************************************************************/
+
 #ifndef CUSTOM_INTERPRETER
 #define CUSTOM_INTERPRETER
 
